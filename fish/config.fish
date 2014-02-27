74cde42c2b4bce4c1c146cf8f436de716169378e
Mateus Kern <kern@mateuskern.com>
2014-02-26 21:40:12 -0300

    Tweak the prompt color; add postgresql start/stop function; add rbenv support;
    Maybe it's time to make smaller commits. :P

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,10 @@ set __fish_git_prompt_showstashstate 'yes'
 # some small function to facilitate the day to day
 function glog; git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative; end
 function gst; git status; end
+
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
+
+function pg_start; launchctl load -w /usr/local/Cellar/postgresql/9.3.2/homebrew.mxcl.postgresql.plist; end
+function pg_stop; launchctl unload -w /usr/local/Cellar/postgresql/9.3.2/homebrew.mxcl.postgresql.plist; end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -18,7 +18,7 @@ end
 
 function fish_prompt --description 'Write out the prompt'
 	printf '%s%s@%s%s %s%s%s%s%s%s $ ' \
-		(set_color white) $USER \
+		(set_color red) $USER \
 		$__fish_prompt_hostname (set_color normal) \
 		(set_color $fish_color_cwd) (prompt_pwd) (set_color normal) \
 	    (set_color yellow) (__fish_git_prompt) (set_color normal)
