19c60f959d6ee9cb5b4f7ef4eaa2a10a97031af5
Mateus Kern <kern@mateuskern.com>
2016-01-27 01:07:55 -0200

    Some cleanup on fish config, add function to start/stop docker-machine and set the enviroment variables, set ruby version to 2.3.0, update $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,11 @@
-set PATH $PATH $HOME/bin $HOME/Code/go/bin
+set PATH $PATH $HOME/Code/dotFiles/bin $HOME/Code/go/bin
 set fish_greeting ""
 set __fish_prompt_hostname (hostname|cut -d . -f 1)
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set -x GOPATH /Users/kern/Code/go
 set -x EDITOR vim
-set -x DOCKER_HOST 'tcp://docker.mateuskern.com:4243'
-#
+
 source ~/Code/dotFiles/fish/shortcuts.fish
 # virtualfish
 set -g PROJECT_HOME $HOME/Code
@@ -15,6 +14,7 @@ eval (python -m virtualfish auto_activation compat_aliases projects)
 # chruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
+chruby ruby-2.3.0
 
 # some small function to facilitate the day to day
 function glog; git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative; end
@@ -22,10 +22,11 @@ function gst; git status; end
 
 function pg_start; pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start; end
 function pg_stop;  pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log stop; end
+function start_docker; docker-machine start $argv[1]; eval (docker-machine env $argv[1]); end
+function stop_docker; docker-machine stop $argv[1]; eval (docker-machine env -u); end
 
-alias docker "docker --tlsverify"
 alias tmux "tmux -2"
 alias vimt "vim -c 'NERDTree|autocmd VimEnter * wincmd p'"
-alias crontab "env EDITOR=vim crontab"
-
+alias cask "brew cask"
+alias airport "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 function reload;source ~/.config/fish/config.fish; end
